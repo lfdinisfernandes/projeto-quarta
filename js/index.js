a8a3836d3579dbfdb702fff98f1de5f8a106fad0
Luiz Felipe <lfdinisfernandes@gmail.com>
2021-09-22 08:16:47 -0300

    aprendendo a usar o html no js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict'
+
+    var $ok =  document.queryselector('[data-js"ok"]')
+   
+     
+    $ok.addEventoListener('click', function(){
+        
+        alert("pego!");
+    });
+    
+})();
